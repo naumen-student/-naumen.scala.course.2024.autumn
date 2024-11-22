9070f26379d57a13f6921db9e48064b02cf6b74c
Kuzmina Lada <92179198+LadaKuzmina@users.noreply.github.com>
2024-11-22 15:30:45 +0500

    Update Test1.scala

--- homeworks/homework_5/src/test/scala/Test1.scala
+++ homeworks/homework_5/src/test/scala/Test1.scala
@@ -1,42 +1,69 @@
-import utest._
+import Task1.ShowSyntax.ShowOps
 
-object Test1 extends TestSuite {
-  val veryLittleCat = "очень маленький кот"
-  val littleCat = "маленький кот"
-  val normalCat = "кот"
-  val bigCat = "большой кот"
-  val veryBigCat = "очень большой кот"
-  val inBox = "в коробке"
+/*
+  Задание №1
+  В задание уже описан тайп класс и синтакс для него.
+  Вам необходимо в объекте ShowInstance описать инстансы тайп класса
+  для типа Cat и Box.
+  Тип Cat, в соответствии с тем, какого конкретно наследника этого типа мы хотим показать,
+  должен отображаться следующим образом:
+  VeryLittleCat - очень маленький кот его_имя
+  LittleCat - маленький кот его_имя
+  NormalCat - кот его_имя
+  BigCat - большой кот его_имя
+  VeryBigCat - очень большой кот его_имя
 
-  override def tests: Tests = Tests {
-    import Task1._
-    import Task1.ShowInstance._
-    import Task1.ShowSyntax._
-    def check[A : Show](toShow: String => A, phrase: String, isInBox: Boolean = false): Unit = {
-      (1 to 5).foreach { _ =>
-        val randomName = java.util.UUID.randomUUID().toString.replace("-", "")
-        assert(
-          toShow(randomName).show == (if (isInBox) s"$phrase $randomName $inBox" else s"$phrase $randomName")
-        )
-      }
+  Если кот будет в коробке, то к тому, что должно выводиться для кота
+  необходимо добавить "в коробке". Если коробка пустая, то выводить "пустая коробка"
+
+  В тестах можно всегда более точно посмотреть фразы.
+ */
+object Task1 extends App {
+  // Обратите внимание, что здесь type class параматрезирован контрвариантно
+  // благодаря этому инстанс Show[Cat] мы можем применить, например, к BigCat.
+  // Однако, это может быть неудобно, так как нам для каждого нового наследника придётся менять
+  // реализацию Show[Cat]. Из-за этого практически все библиотеки, которые предоставляют свои
+  // тайп классы для их использования пользователем, делают тайп классы инвариантными по параметру.
+  // Можете написать две реализации (при -А и А) и сравнить их.
+  trait Show[-A] {
+    def show(a: A): String
+  }
+
+  sealed trait Cat {
+    def name: String
+  }
+  case class VeryLittleCat(name: String) extends Cat
+  case class LittleCat(name: String) extends Cat
+  case class NormalCat(name: String) extends Cat
+  case class BigCat(name: String) extends Cat
+  case class VeryBigCat(name: String) extends Cat
+
+  sealed trait Box[+A] {
+    def value: A
+  }
+  case class BoxWith[+A](value: A) extends Box[A]
+  case object EmptyBox extends Box[Nothing] {
+    override def value: Nothing = throw new Exception("Empty box!")
+  }
+
+  object ShowInstance {
+    implicit val catShow: Show[Cat] = {
+      case VeryLittleCat(name) => s"очень маленький кот $name"
+      case LittleCat(name) => s"маленький кот $name"
+      case NormalCat(name) => s"кот $name"
+      case BigCat(name) => s"большой кот $name"
+      case VeryBigCat(name) => s"очень большой кот $name"
     }
 
-    'showCats - {
-      'veryLittleCat - check(VeryLittleCat, veryLittleCat)
-      'littleCat - check(LittleCat, littleCat)
-      'normalCat - check(NormalCat, normalCat)
-      'bigCat - check(BigCat, bigCat)
-      'veryBigCat - check(VeryBigCat, veryBigCat)
+    implicit def boxShow[A: Show]: Show[Box[A]] = {
+      case BoxWith(value) => s"${value.show} в коробке"
+      case EmptyBox => "пустая коробка"
     }
-    'showBox - {
-      'emptyBox - assert(EmptyBox.show == "пустая коробка")
-      'catInBox - {
-        'veryLittleCat - check(name => BoxWith(VeryLittleCat(name)), veryLittleCat, true)
-        'littleCat - check(name => BoxWith(LittleCat(name)), littleCat, true)
-        'normalCat - check(name => BoxWith(NormalCat(name)), normalCat, true)
-        'bigCat - check(name => BoxWith(BigCat(name)), bigCat, true)
-        'veryBigCat - check(name => BoxWith(VeryBigCat(name)), veryBigCat, true)
-      }
+  }
+
+  object ShowSyntax {
+    implicit class ShowOps[A](val a: A) {
+      def show(implicit show: Show[A]): String = show.show(a)
     }
   }
 }
