ddadeeb4736bb5f24f1baf228991df658d3e6e91
Ivan Sychev <86532587+funov@users.noreply.github.com>
2024-11-22 17:25:20 +0500

    Update Test5.scala

--- homeworks/homework_5/src/test/scala/Test5.scala
+++ homeworks/homework_5/src/test/scala/Test5.scala
@@ -1,66 +1,62 @@
-import utest._
+import scala.util.{Failure, Success, Try}
 
-import scala.util.Random
+/*
+  Задание №5
+  Задание аналогично предыдущему задания, но теперь мы уходим от использования стандартного Either.
+  Нужно:
+  1) Доделать реализацию MyEither (нужны аналоги Right и Left)
+  2) Написать для MyEither инстанс MonadError
+  3) Написать функции apply, error, possibleError
+ */
+object Task5 extends App {
+  import Task4.MonadError
 
-object Test5 extends TestSuite {
-  override def tests: Tests = Tests {
-    import Task5._
-    import Task5.MyEitherSyntax._
+  sealed trait MyEither[+E, +A] {
+    def isError: Boolean
+  }
 
-    'leftIdentityLow - {
-      assert(MyEither(0).flatMap(x => MyEither(x + 1)) == MyEither(1))
+  case class MyRight[+A](value: A) extends MyEither[Nothing, A] {
+    def isError = false
+  }
+
+  case class MyLeft[+E](error: E) extends MyEither[E, Nothing] {
+    def isError = true
+  }
+
+  object MyEither {
+    def apply[A](value: A): MyEither[Nothing, A] = MyRight(value)
+    def error[E, A](error: E): MyEither[E, A] = MyLeft(error)
+    def possibleError[A](f: => A): MyEither[Throwable, A] = Try(f) match {
+      case Success(value) => MyRight(value)
+      case Failure(error) => MyLeft(error)
     }
-    'rightIdentityLow - {
-      assert(MyEither(0).flatMap(x => MyEither.apply(x)) == MyEither(0))
-    }
-    'associativityLow - {
-      val f: Int => MyEither[Nothing, Int] = x => MyEither(x + 1)
-      val g: Int => MyEither[Nothing, Int] = x => MyEither(x * 10)
-      assert(MyEither(0).flatMap(f).flatMap(g) == MyEither(0).flatMap(x => f(x).flatMap(g)))
-    }
-    'usage - {
-      'simpleUsage - {
-        val (x, y, z) = (Random.nextInt(), Random.nextInt(), Random.nextInt())
-        val p = for {
-          a <- MyEither(x)
-          c <- MyEither(y)
-          d <- MyEither(z)
-        } yield a + c + d
-        assert(p == MyEither(x + y + z))
-      }
-      'errorUsage - {
-        val p = for {
-          a <- MyEither(0)
-          _ <- MyEither.error[String, Int]("Error")
-        } yield a
-        assert(p.isError)
-      }
-      'possibleErrorUsage - {
-        val p = for {
-          a <- MyEither(12)
-          b <- MyEither.possibleError(12 / 0)
-        } yield a + b
-        assert(p.isError)
-      }
-      'recoverErrorUsage - {
-        val (x, y, z) = (Random.nextInt(), Random.nextInt(), Random.nextInt())
-        val p = for {
-          a <- MyEither(x)
-          b <- MyEither.possibleError(y / 0).handleError(_ => y)
-          c <- MyEither(z)
-        } yield a + b + c
 
-        assert(p == MyEither(x + y + z))
+    implicit def myEitherMonad[E]: MonadError[MyEither, E] = new MonadError[MyEither, E] {
+      def pure[A](value: A): MyEither[E, A] = MyRight(value)
+
+      def flatMap[A, B](fa: MyEither[E, A])(f: A => MyEither[E, B]): MyEither[E, B] = fa match {
+        case MyRight(value) => f(value)
+        case MyLeft(error) => MyLeft(error)
       }
-      'skippingErrorStopsEvaluation - {
-        val p = for {
-          a <- MyEither(0)
-          v <- MyEither.possibleError(12 / 0)
-          b <- MyEither.possibleError(32 / 12).handleError(_ => 1)
-          c <- MyEither(2)
-        } yield a + b + c + v
-        assert(p.isError)
+
+      def raiseError[A](f: MyEither[E, A])(error: => E): MyEither[E, A] = MyLeft(error)
+
+      def handleError[A](f: MyEither[E, A])(handle: E => A): MyEither[E, A] = f match {
+        case MyRight(value) => MyRight(value)
+        case MyLeft(error) => MyRight(handle(error))
       }
+    }
+  }
+
+  object MyEitherSyntax {
+    implicit class MyEitherOps[E, A](val either: MyEither[E, A]) {
+      def flatMap[B](f: A => MyEither[E, B]): MyEither[E, B] =
+        MyEither.myEitherMonad[E].flatMap(either)(f)
+
+      def map[B](f: A => B): MyEither[E, B] = MyEither.myEitherMonad.map(either)(f)
+
+      def handleError(f: E => A): MyEither[E, A] =
+        MyEither.myEitherMonad.handleError(either)(f)
     }
   }
 }
