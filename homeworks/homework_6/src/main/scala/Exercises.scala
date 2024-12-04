17a29ddf490f16079b620869c57ea520aaf493b2
Vearsmon <vearsmon285537@gmail.com>
2024-12-04 14:33:10 +0500

    homework 6: complete

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -12,7 +12,7 @@ object Exercises {
      * вернулся None, а в случае упеха Some
      */
     def task1(r: Int, g: Int, b: Int): URIO[ColorService, Option[Color]] =
-        ZIO.serviceWithZIO[ColorService](_.getColor(r, g, b))
+        ZIO.serviceWithZIO[ColorService](_.getColor(r, g, b)).option
 
 
     /**
@@ -23,6 +23,13 @@ object Exercises {
      */
     def task2(size: (Int, Int)): ZIO[PictureGenerationService, GenerationError, String] =
         ZIO.serviceWithZIO[PictureGenerationService](_.generatePicture(size))
+          .map(el => el.lines
+            .map(str => str
+              .map(color => Integer.toUnsignedString(color.getRGB))
+              .mkString(" ")
+            )
+            .mkString("\n")
+          )
 
 
     /**
@@ -37,15 +44,15 @@ object Exercises {
         for {
             colorServ <- ZIO.service[ColorService]
             pictureServ <- ZIO.service[PictureGenerationService]
-            color <- colorServ.generateRandomColor()
-            picture <- pictureServ.generatePicture(size)
-            filledPicture <- pictureServ.fillPicture(picture, color)
+            color <- colorServ.generateRandomColor().mapError(err => new GenerationError("Не удалось создать цвет"))
+            picture <- pictureServ.generatePicture(size).mapError(err => new GenerationError("Ошибка генерации изображения"))
+            filledPicture <- pictureServ.fillPicture(picture, color).mapError(err => new GenerationError("Возникли проблемы при заливке изображения"))
         } yield filledPicture
 
     /**
      * Необходимо предоставить объекту ZIO все необходимые зависимости
      */
     def task4(size: (Int, Int)): IO[GenerationError, Picture] =
-        task3(size)
+        task3(size).provide(utils.ColorService.live ++ (utils.ColorService.live >>> utils.PictureGenerationService.live))
 
 }
