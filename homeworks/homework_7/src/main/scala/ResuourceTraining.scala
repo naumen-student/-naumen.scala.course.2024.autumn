5c01d75d2091b855c600bd0abea90f5cc58bd5ef
Konstantin Zaikin <zaikinke@yandex.ru>
2024-12-08 21:28:04 +0500

    Done homework_7

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -13,9 +13,38 @@ import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
 
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+    def readData(filePath: String): IO[Throwable, String] = {
+        val acquire = ZIO.attempt(new BufferedReader(new FileReader(filePath)))
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+        def release(reader: BufferedReader) = ZIO.attempt(reader.close()).orDie
 
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
+        def read(reader: BufferedReader) = ZIO.attempt {
+            val sb = new StringBuilder()
+            var line: String = null
+            while ( {
+                line = reader.readLine();
+                line != null
+            }) {
+                sb.append(line).append(System.lineSeparator())
+            }
+            sb.toString().stripTrailing()
+        }
+
+        ZIO.acquireReleaseWith(acquire)(release)(read)
+    }
+
+    def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+        val acquire = ZIO.attempt(new BufferedWriter(new FileWriter(filePath)))
+
+        def release(writer: BufferedWriter) = ZIO.attempt(writer.close()).orDie
+
+        def write(writer: BufferedWriter) = ZIO.attempt {
+            writer.write(data)
+            writer.flush()
+        }.orDie
+
+        ZIO.acquireReleaseWith(acquire)(release)(write).orElse(ZIO.unit)
+    }
+
+    override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
