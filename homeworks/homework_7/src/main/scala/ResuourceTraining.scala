75176a59ccf9252de742d7c7f5060acc78b217f7
balbesi4 <arseny.elkin@urfu.me>
2024-12-13 18:24:27 +0500

    homework_7 commpleted

--- homeworks/homework_7/src/main/scala/ResuourceTraining.scala
+++ homeworks/homework_7/src/main/scala/ResuourceTraining.scala
@@ -1,21 +1,41 @@
 package ru.dru
 
 import zio.{IO, Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
-
 import java.io.{BufferedReader, BufferedWriter, FileReader, FileWriter}
-
-
-/**
- * Необходимо реализовать функции readData и writeData, записывающие и читающие данные в/из файла соответственно.
- * В реализации следует применять безопасное использование ресурсов ZIO.acquireReleaseWith
- */
-
 
 object ResuourceTraining extends ZIOAppDefault {
 
-  def readData(filePath: String): IO[Throwable, String] = ???
+  /**
+   * Читает данные из файла по заданному пути.
+   * Использует безопасное управление ресурсами с помощью ZIO.acquireReleaseWith.
+   *
+   * @param filePath путь к файлу для чтения
+   * @return Содержимое файла как строка
+   */
+  def readData(filePath: String): IO[Throwable, String] = {
+    ZIO.acquireReleaseWith(
+        ZIO.attempt(new BufferedReader(new FileReader(filePath))))(
+      reader => ZIO.attempt(reader.close()).orDie)(
+      reader => ZIO.attempt(reader.readLine()))
+  }
 
-  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = ???
+  /**
+   * Записывает данные в файл по заданному пути.
+   * Использует безопасное управление ресурсами с помощью ZIO.acquireReleaseWith.
+   *
+   * @param filePath путь к файлу для записи
+   * @param data     данные для записи
+   * @return ZIO эффект, завершающийся Unit
+   */
+  def writeData(filePath: String, data: String): ZIO[Any, Nothing, Unit] = {
+    ZIO.acquireReleaseWith(
+        ZIO.attempt(new BufferedWriter(new FileWriter(filePath))))(
+        writer => ZIO.attempt(writer.close()).orDie)(
+      writer => ZIO.attempt{
+        writer.write(data)
+        writer.flush()
+      }).orDie
+  }
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ZIO.succeed("Done")
 }
