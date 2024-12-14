8184c9a38b048cdf7c51a34d1b3bb8cdc5440d1e
Lost-Fly <karamyshevvlad99@gmail.com>
2024-12-14 19:41:59 +0500

    homework_5 - Карамышев Владислав

--- homeworks/homework_5/src/main/scala/Task3.scala
+++ homeworks/homework_5/src/main/scala/Task3.scala
@@ -3,7 +3,7 @@ import cats.implicits._
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{Duration, DurationInt}
 
 /*
   Задание №3
@@ -25,8 +25,35 @@ object Task3 extends App {
   case class Count(word: String, count: Int)
   case class WordsCount(count: Seq[Count])
   object WordsCount {
-    implicit val monoid: Monoid[WordsCount] = ???
+    implicit val monoid: Monoid[WordsCount] = new Monoid[WordsCount] {
+      override def empty: WordsCount = new WordsCount(count = Seq[Count]())
+
+      override def combine(x: WordsCount, y: WordsCount): WordsCount = {
+        val combinedCounts = (x.count ++ y.count)
+          .groupBy(_.word)
+          .map { case (word, counts) =>
+            Count(word, counts.map(_.count).sum)
+          }
+          .toSeq
+
+        WordsCount(combinedCounts)
+      }
+    }
   }
 
-  def countWords(lines: Vector[String]): WordsCount = ???
+  def countWords(lines: Vector[String]): WordsCount = {
+    val ft = mapReduce[String, WordsCount](lines) { line =>
+      val wordCounts = line.split(' ').toVector
+        .groupBy(identity)
+        .map { case (word, occurrences) =>
+          Count(word, occurrences.length)
+        }
+        .toSeq
+
+      WordsCount(wordCounts)
+    }
+
+    Await.result(ft, Duration.Inf)
+  }
+
 }
